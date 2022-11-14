0db47f2e2a3e94b0c466bf22d37c16dcc94be81a
v-dev <v-dev@users.noreply.github.com>
2022-11-13 23:26:09 -0800

    updatel sql_queries per code review feedback
    reformat sql, add precision to numeric duration, users insert do update on conflict

--- projects/1_data-modeling-postgres/sql_queries.py
+++ projects/1_data-modeling-postgres/sql_queries.py
@@ -1,66 +1,66 @@
 # DROP TABLES
 
-songplay_table_drop = "drop table if exists songplays"
-user_table_drop = "drop table if exists users"
-song_table_drop = "drop table if exists songs"
-artist_table_drop = "drop table if exists artists"
-time_table_drop = "drop table if exists time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
-create table if not exists songplays (
-songplay_id int primary key,
-start_time timestamp NOT NULL,
-user_id int NOT NULL,
-level varchar(100),
-song_id varchar(100),
-artist_id varchar(100),
-session_id int,
-location varchar(500),
-user_agent text
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id INT PRIMARY KEY,
+    start_time  TIMESTAMP NOT NULL,
+    user_id     INT       NOT NULL,
+    level       VARCHAR(100),
+    song_id     VARCHAR(100),
+    artist_id   VARCHAR(100),
+    session_id  INT,
+    location    VARCHAR(500),
+    user_agent  TEXT
 );
 """)
 
 user_table_create = ("""
-create table if not exists users (
-user_id int primary key,
-first_name varchar(500),
-last_name varchar(500),
-gender varchar(100),
-level varchar(100)
-)
+CREATE TABLE IF NOT EXISTS users (
+    user_id    INT PRIMARY KEY,
+    first_name VARCHAR(500),
+    last_name  VARCHAR(500),
+    gender     VARCHAR(100),
+    level      VARCHAR(100)
+);
 """)
 
 song_table_create = ("""
-create table if not exists songs (
-song_id varchar(100) primary key,
-title text NOT NULL ,
-artist_id varchar(100),
-year int,
-duration numeric NOT NULL
+CREATE TABLE IF NOT EXISTS songs (
+    song_id   VARCHAR(100) PRIMARY KEY,
+    title     TEXT    NOT NULL,
+    artist_id VARCHAR(100),
+    year      INT,
+    duration  NUMERIC(8,5) NOT NULL
 );
 """)
 
 artist_table_create = ("""
-create table if not exists artists (
-artist_id varchar(100) primary key,
-name varchar(500) NOT NULL,
-location varchar(500),
-latitude double precision,
-longitude double precision
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id VARCHAR(100) PRIMARY KEY,
+    name      VARCHAR(500) NOT NULL,
+    location  VARCHAR(500),
+    latitude  DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
 );
 """)
 
 time_table_create = ("""
-create table if not exists time (
-start_time timestamp,  -- maybe this should be a bigint or varchar? it's epoch format
-hour int,
-day int,
-week int,
-month varchar(20),
-year varchar(20),
-weekday varchar(20)
+CREATE TABLE IF NOT EXISTS time (
+    start_time TIMESTAMP,
+    hour       INT,
+    day        INT,
+    week       INT,
+    month      VARCHAR(20),
+    year       VARCHAR(20),
+    weekday    VARCHAR(20)
 );
 """)
 
@@ -68,44 +68,49 @@ weekday varchar(20)
 
 # 9 columns for songplays table
 songplay_table_insert = ("""
-INSERT INTO songplays VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+INSERT INTO songplays
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
 ON CONFLICT (songplay_id) 
 DO NOTHING
 """)
 
 # 5 columns for users table
 user_table_insert = ("""
-INSERT INTO users VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (user_id) 
-DO NOTHING
+INSERT INTO users
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT(user_id)
+DO UPDATE SET level=EXCLUDED.level; 
 """)
 
 song_table_insert = ("""
-insert into songs values (%s, %s, %s, %s, %s)
+INSERT INTO songs
+VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT (song_id) 
 DO NOTHING
 """)
 
 artist_table_insert = ("""
-insert into artists values (%s, %s, %s, %s, %s)
+INSERT INTO artists
+VALUES (%s, %s, %s, %s, %s)
 ON CONFLICT (artist_id) 
 DO NOTHING
 """)
 
-
 time_table_insert = ("""
-insert into time values (%s, %s, %s, %s, %s, %s, %s)
+INSERT INTO time
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 """)
 
 # FIND SONGS
 
 song_select = ("""
-select song_id, a.artist_id
-from songs s
-join artists a on s.artist_id = a.artist_id
-where s.title = %s
-and a.name = %s
-and s.duration = %s
+SELECT song_id, a.artist_id
+  FROM songs s
+       JOIN artists a
+       ON s.artist_id = a.artist_id
+ WHERE s.title = %s
+   AND a.name = %s
+   AND s.duration = %s
 """)
 
 # QUERY LISTS
