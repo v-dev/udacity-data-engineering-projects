aefedf45bb5516cc84370b5ffcfeb3c3c5136b09
v-dev <v_dev@live.com>
2022-12-25 02:33:57 -0800

    reformat, add simple directions to readme

--- projects/2_data_warehouse/README.md
+++ projects/2_data_warehouse/README.md
@@ -1,8 +1,16 @@
 
+# How to run
+(Re)create the tables:
+
+    python create_tables.py
+
+Run the ETL process:
+
+    python etl.py
+
 
 # TODO
 1. fill up this readme
 2. optimize table creates with dist & sortkeys
 3. look at rubric
 4. tbd
-
--- projects/2_data_warehouse/create_tables.py
+++ projects/2_data_warehouse/create_tables.py
@@ -1,5 +1,7 @@
 import configparser
+
 import psycopg2
+
 from sql_queries import create_table_queries, drop_table_queries
 
 
@@ -20,22 +22,23 @@ def get_config_data():
     config.read('dwh.cfg')
 
     return {
-        'host': config.get("CLUSTER","HOST"),
-        'dbname': config.get("CLUSTER","DB_NAME"),
-        'user': config.get("CLUSTER","DB_USER"),
-        'password': config.get("CLUSTER","DB_PASSWORD"),
-        'port': int(config.get("CLUSTER","DB_PORT")),
+        'host': config.get("CLUSTER", "HOST"),
+        'dbname': config.get("CLUSTER", "DB_NAME"),
+        'user': config.get("CLUSTER", "DB_USER"),
+        'password': config.get("CLUSTER", "DB_PASSWORD"),
+        'port': int(config.get("CLUSTER", "DB_PORT")),
 
         # TODO: possibly not needed here and can be removed
-        'arn': config.get("IAM_ROLE","ARN"),
-        'log_data': config.get("S3","LOG_DATA"),
-        'song_data': config.get("S3","SONG_DATA")
+        'arn': config.get("IAM_ROLE", "ARN"),
+        'log_data': config.get("S3", "LOG_DATA"),
+        'song_data': config.get("S3", "SONG_DATA")
     }
 
 
 def main():
     data = get_config_data()
-    conn = psycopg2.connect(f"host={data['host']} dbname={data['dbname']} user={data['user']} password={data['password']} port={data['port']}")
+    conn = psycopg2.connect(
+        f"host={data['host']} dbname={data['dbname']} user={data['user']} password={data['password']} port={data['port']}")
     cur = conn.cursor()
 
     drop_tables(cur, conn)
